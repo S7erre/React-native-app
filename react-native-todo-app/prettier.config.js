43e156b719b5fad5da3ef6e0cf7957fd9758690d
s7erre <18643318+S7erre@users.noreply.github.com>
2022-02-21 23:52:50 +0100

    expo project

---
+++ react-native-todo-app/prettier.config.js
@@ -0,0 +1,11 @@
+const option = {
+    arrowParens: 'avoid',
+    singleQuote: true,
+    backSpacing: true,
+    endOfLine: 'lf',
+    sem: false,
+    tabWidth: 2,
+    trailingComma: 'none'
+}
+
+module.exports = options
